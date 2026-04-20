4535bb2ec59c6be1a5b3bc8bf21d7d6dee7328a8
Raveendran Sinthuja <sinthujaravi96@gmail.com>
2026-04-20 18:33:15 +0100

    CREATE Member model

---
+++ src/main/java/resources/Models/Member.java
@@ -0,0 +1,39 @@
+package resources.Models;
+
+import java.util.Objects;
+
+public class Member {
+    private String id;
+    private String name;
+
+    public Member(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    
+    @Override
+    public String toString() {
+        return name + " (" + id + ")";
+    }
+}
