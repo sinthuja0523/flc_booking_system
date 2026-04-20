6be2c0e163aabe04e175e96aeba6a01872f245f8
Raveendran Sinthuja <sinthujaravi96@gmail.com>
2026-04-20 18:39:37 +0100

    CREATE Booking model

---
+++ src/main/java/resources/Models/Booking.java
@@ -0,0 +1,33 @@
+package flc.models;
+
+public class Booking {
+    private String bookingId;
+    private Member member;
+    private Lesson lesson;
+    private BookingStatus status;
+
+    public Booking(String bookingId, Member member, Lesson lesson) {
+        this.bookingId = bookingId;
+        this.member = member;
+        this.lesson = lesson;
+        this.status = BookingStatus.BOOKED;
+    }
+
+    public String getBookingId() { return bookingId; }
+    public Member getMember() { return member; }
+    public Lesson getLesson() { return lesson; }
+    public BookingStatus getStatus() { return status; }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+
+    public void setStatus(BookingStatus status) {
+        this.status = status;
+    }
+    
+    @Override
+    public String toString() {
+        return "Booking " + bookingId + ": " + member.getName() + " -> " + lesson.getExerciseType() + " [" + status + "]";
+    }
+}
